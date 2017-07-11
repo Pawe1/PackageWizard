8e46f73d5a567849f1259028bd6357de7e6ecb1a
PC <Pawe1@users.noreply.github.com>
2017-07-11 09:46:24 +0200

    Drafting

--- Main.pas
+++ Main.pas
@@ -5,7 +5,9 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TabControl,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, System.ImageList,
+  FMX.ImgList, FMX.ListView, FMX.ListBox, FMX.ComboEdit, FMX.Edit;
 
 type
   TForm1 = class(TForm)
@@ -14,6 +16,39 @@ type
     TabControl1: TTabControl;
     TabItem1: TTabItem;
     TabItem2: TTabItem;
+    Edit1: TEdit;
+    Button3: TButton;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    ComboEdit1: TComboEdit;
+    GroupBox1: TGroupBox;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    CheckBox4: TCheckBox;
+    CheckBox5: TCheckBox;
+    CheckBox6: TCheckBox;
+    CheckBox7: TCheckBox;
+    ComboBox1: TComboBox;
+    ComboBox2: TComboBox;
+    ComboBox3: TComboBox;
+    ComboBox4: TComboBox;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    GroupBox2: TGroupBox;
+    ListView1: TListView;
+    ImageList1: TImageList;
+    ImageList2: TImageList;
+    Label1: TLabel;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    OpenDialog: TOpenDialog;
+    SaveDialog: TSaveDialog;
+    OpenProjectDialog: TOpenDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -26,5 +61,29 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if OpenDialog.Execute then
+  begin
+
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  if SaveDialog.Execute then
+  begin
+
+  end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  if OpenProjectDialog.Execute then
+  begin
+
+  end;
+end;
 
 end.
